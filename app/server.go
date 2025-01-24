c4ef1be55f6da54e52f3b7fcaa14cd94ad3f0e0b
muhuchah <m.h.chah4@gmail.com>
2025-01-24 01:19:26 -0500

    Write sth in conn

--- app/server.go
+++ app/server.go
@@ -20,10 +20,18 @@ func main() {
 	  fmt.Println("Failed to bind to port 4221")
 	 	os.Exit(1)
   }
+  defer l.Close()
 	
-	_, err = l.Accept()
+  conn, err := l.Accept()
 	if err != nil {
 	 	fmt.Println("Error accepting connection: ", err.Error())
 	 	os.Exit(1)
 	}
+
+  response := "HTTP/1.1 200 OK\r\n\r\n"
+  _, err = conn.Write([]byte(response))
+  if err != nil {
+    fmt.Println("Error writing in connection: ", err.Error())
+  }
+
 }
