f0c9e2ed7e0419fc9c89eca42c587642c02ccac8
muhuchah <m.h.chah4@gmail.com>
2025-01-24 01:39:17 -0500

    Check the path

--- app/server.go
+++ app/server.go
@@ -1,20 +1,44 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
 
-// Ensures gofmt doesn't remove the "net" and "os" imports above (feel free to remove this!)
-var _ = net.Listen
-var _ = os.Exit
+
+func handleConnection(conn net.Conn) {
+  defer conn.Close()
+
+  reader := bufio.NewReader(conn)
+  message, err := reader.ReadString('\n')
+  if err != nil {
+    fmt.Println("Error reading: ", err)
+    return
+  }
+
+  fmt.Print("Message received: ", string(message))
+  header := strings.Split(message, " ")
+
+  fmt.Println(header[1])
+
+  response := ""
+  if header[1] == "/" {
+    response = "HTTP/1.1 200 OK\r\n\r\n"
+  } else {
+    response = "HTTP/1.1 404 Not Found\r\n\r\n"
+  }
+
+  _, err = conn.Write([]byte(response))
+  if err != nil {
+    fmt.Println("Error writing in connection: ", err.Error())
+  }
+}
+
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-
-  // Uncomment this block to pass the first stage
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 	  fmt.Println("Failed to bind to port 4221")
@@ -28,10 +52,6 @@ func main() {
 	 	os.Exit(1)
 	}
 
-  response := "HTTP/1.1 200 OK\r\n\r\n"
-  _, err = conn.Write([]byte(response))
-  if err != nil {
-    fmt.Println("Error writing in connection: ", err.Error())
-  }
+  handleConnection(conn)
 
 }
