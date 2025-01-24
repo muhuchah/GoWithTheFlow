86395a5df6c618c661e0a25e814f36025ffd419c
muhuchah <m.h.chah4@gmail.com>
2025-01-24 02:49:19 -0500

    feat(static): Now we handle static files :))

--- app/server.go
+++ app/server.go
@@ -9,32 +9,63 @@ import (
 )
 
 
+func handleContentType(filePath string) string {
+  contentType := "text/plain"
+  if strings.HasSuffix(filePath, ".html") {
+    contentType = "text/html"
+  } else if strings.HasSuffix(filePath, ".css") {
+    contentType = "text/css"
+  } else if strings.HasSuffix(filePath, ".js") {
+    contentType = "application/javascript"
+  }
+
+  return contentType
+}
+
+
 func handleConnection(conn net.Conn) {
   defer conn.Close()
 
   reader := bufio.NewReader(conn)
-  message, err := reader.ReadString('\n')
+  requestLine, err := reader.ReadString('\n')
   if err != nil {
     fmt.Println("Error reading: ", err)
     return
   }
 
-  fmt.Print("Message received: ", string(message))
-  header := strings.Split(message, " ")
+  fmt.Print("Message received: ", string(requestLine))
+  headerParts := strings.Split(requestLine, " ")
+  if len(headerParts) < 2 {
+    fmt.Println("Invalid request")
+    return
+  }
+  
+  method := headerParts[0]
+  path := headerParts[1]
 
-  fmt.Println(header[1])
+  if method == "GET" {
+    filePath := "static" + path
+    if path == "/" {
+      filePath = "static/index.html"
+    }
 
-  response := ""
-  if header[1] == "/" {
-    response = "HTTP/1.1 200 OK\r\n\r\n"
+    file, err := os.ReadFile(filePath)
+    if err != nil {
+      // File not found
+      response := "HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\n\r\n404 Not Found"
+      conn.Write([]byte(response))
+      return
+    }
+
+    ContentType := handleContentType(filePath)
+
+    response := fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: %s\r\nContent-Length: %d\r\n\r\n%s", ContentType, len(file), file)
+    conn.Write([]byte(response))
   } else {
-    response = "HTTP/1.1 404 Not Found\r\n\r\n"
+    response := "HTTP/1.1 405 Method Not Allowed\r\nContent-Type: text/plain\r\n\r\n405 Method Not Allowed"
+		conn.Write([]byte(response))
   }
 
-  _, err = conn.Write([]byte(response))
-  if err != nil {
-    fmt.Println("Error writing in connection: ", err.Error())
-  }
 }
 
 
@@ -57,3 +88,4 @@ func main() {
   }
 
 }
+
